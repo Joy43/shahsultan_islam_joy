f84d0a2bc5f2874e960ffc90c60406b3c0f903b1
Joy43 <ssjoy43@gmail.com>
2025-05-24 00:57:15 +0600

    adding url category

--- src/components/modules/blog/blogDetails/index.tsx
+++ src/components/modules/blog/blogDetails/index.tsx
@@ -8,7 +8,7 @@ const BlogDetails = ({ blog }: { blog: IBlogs }) => {
     <article className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
       <div className="mb-8">
         <Link 
-          href="/blogs" 
+          href="/blog" 
           className="inline-flex items-center text-gray-600 hover:text-blue-600 transition-colors"
         >
           <FiArrowLeft className="w-5 h-5 mr-2" />
