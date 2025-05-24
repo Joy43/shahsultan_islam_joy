529a4f9edc9e0a6408d62828e125051bd91fed4f
Joy43 <ssjoy43@gmail.com>
2025-05-24 15:34:44 +0600

    blog page deploy sove

--- src/app/(WithCommonLayout)/blog/page.tsx
+++ src/app/(WithCommonLayout)/blog/page.tsx
@@ -1,14 +1,8 @@
+
 import AllBlogs from "@/components/modules/blog";
 import { getAllBlog } from "@/services/blog";
 
-
-const Blogpages = async() => {
-    const {data:blogs}=await getAllBlog();
-  return (
-    <>
-    <AllBlogs blogs={blogs}/>
-    </>
-  )
+export default async function BlogPage() {
+  const { data: blogs } = await getAllBlog();
+  return <AllBlogs blogs={blogs} />;
 }
-
-export default Blogpages;
