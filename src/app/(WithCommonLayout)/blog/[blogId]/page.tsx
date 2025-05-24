7074f1381ea7951206abba253f62844bbd3a0f65
Joy43 <ssjoy43@gmail.com>
2025-05-24 21:48:28 +0600

    es lint solve

--- src/app/(WithCommonLayout)/blog/[blogId]/page.tsx
+++ src/app/(WithCommonLayout)/blog/[blogId]/page.tsx
@@ -1,20 +1,23 @@
+// app/(WithCommonLayout)/blog/[blogId]/page.tsx
+
 import BlogDetails from "@/components/modules/blog/blogDetails";
 import { getSingleBlog } from "@/services/blog";
-const BlogDetailsPage = async ({
-   params 
-  }: {
-    params:Promise<{blogId:string}>;
-  })=>{
-    const {blogId}=await params ;
-    const {data:blog}=await getSingleBlog(blogId)
-  
 
-    return (
-      <main className="bg-gray-50 min-h-screen">
-        <BlogDetails blog={blog} />
-      </main>
-    );
-  
+type BlogDetailsPageProps = {
+  params: {
+    blogId: string;
+  };
+};
+
+const BlogDetailsPage = async ({ params }: BlogDetailsPageProps) => {
+  const { blogId } = params;
+  const { data: blog } = await getSingleBlog(blogId);
+
+  return (
+    <main className="bg-gray-50 min-h-screen">
+      <BlogDetails blog={blog} />
+    </main>
+  );
 };
 
 export default BlogDetailsPage;
