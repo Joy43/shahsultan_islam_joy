39d042cd053aa526a729a8cc938eb0bb24b99574
Joy43 <ssjoy43@gmail.com>
2025-05-24 22:50:12 +0600

    try to solve route

--- src/app/(WithCommonLayout)/blog/[blogId]/page.tsx
+++ src/app/(WithCommonLayout)/blog/[blogId]/page.tsx
@@ -1,23 +1,22 @@
-// app/(WithCommonLayout)/blog/[blogId]/page.tsx
-
 import BlogDetails from "@/components/modules/blog/blogDetails";
 import { getSingleBlog } from "@/services/blog";
 
-type BlogDetailsPageProps = {
-  params: {
-    blogId: string;
-  };
-};
 
-const BlogDetailsPage = async ({ params }: BlogDetailsPageProps) => {
-  const { blogId } = params;
+const blogDetailsPage = async ({
+  params,
+}: {
+  params: Promise<{ blogId: string }>;
+}) => {
+  const { blogId } = await params;
+
   const { data: blog } = await getSingleBlog(blogId);
 
   return (
-    <main className="bg-gray-50 min-h-screen">
-      <BlogDetails blog={blog} />
-    </main>
+    <div>
+     
+      <BlogDetails blog ={blog } />
+    </div>
   );
 };
 
-export default BlogDetailsPage;
+export default blogDetailsPage;
